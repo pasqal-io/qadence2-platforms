f454cb7dbd8c659384b8f433578d7cce5d84ff10
seitzdom <dominik.seitz@pasqal.com>
2024-06-26 13:45:12 +0200

    add api

---
+++ qadence2_platforms/backend/api.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from collections import Counter
+from importlib import import_module
+from logging import getLogger
+
+import pyqtorch as pyq
+import torch
+
+from qadence2_platforms.backend.pyqtorch.embed import Embedding
+from qadence2_platforms.qadence_ir import Model
+
+logger = getLogger(__name__)
+
+
+class Api(torch.nn.Module):
+    """A class holding the final embedding instance and the pyq.QuantumCircuit."""
+
+    def __init__(
+        self,
+        embedding: Embedding,
+        circuit: pyq.QuantumCircuit,
+        observable: pyq.Observable = None,
+        backend: str = "pyqtorch",
+    ) -> None:
+        super().__init__()
+        self.embedding = embedding
+        self.circuit = circuit
+        self.observable = observable
+        self.backend = backend
+
+    def forward(
+        self, state: torch.Tensor, inputs: dict[str, torch.Tensor]
+    ) -> torch.Tensor:
+        return self.run(state, inputs)
+
+    def run(self, state: torch.Tensor, inputs: dict[str, torch.Tensor]) -> torch.Tensor:
+        return pyq.run(self.circuit, state, self.embedding(inputs))
+
+    def sample(
+        self, state: torch.Tensor, inputs: dict[str, torch.Tensor], n_shots: int = 1000
+    ) -> list[Counter]:
+        return pyq.sample(self.circuit, state, self.embedding(inputs), n_shots)  # type: ignore[no-any-return]
+
+    def expectation(
+        self, state: torch.Tensor, inputs: dict[str, torch.Tensor]
+    ) -> torch.Tensor:
+        return pyq.expectation(
+            self.circuit, state, self.embedding(inputs), self.observable
+        )
+
+
+def compile(model: Model, backend: str) -> Api:
+    embed = import_module("qadence2_platforms.backend.pyqtorch.embed")
+    compiler = import_module("qadence2_platforms.backend.pyqtorch.compile")
+    embedding = embed.Embedding(model)
+    native_circ = compiler.compile(model)
+    return Api(embedding, native_circ, backend)
--- tests/test_pyq_compiler.py
+++ tests/test_pyq_compiler.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 import pyqtorch as pyq
 import torch
 
-from qadence2_platforms.backend.pyqtorch.compile import PyqModel
-from qadence2_platforms.backend.pyqtorch.compile import compile as pyq_compile
+from qadence2_platforms.backend.api import compile
 from qadence2_platforms.qadence_ir import (
     Alloc,
     AllocQubits,
@@ -34,7 +33,7 @@ def test_pyq_compilation() -> None:
         directives={"digital": True},
         data_settings={"result-type": "state-vector", "data-type": "f32"},
     )
-    compiled_model: PyqModel = pyq_compile(model)
+    compiled_model = compile(model, "pyqtorch")
     f_params = {"x": torch.rand(1, requires_grad=True)}
     wf = compiled_model(pyq.zero_state(2), f_params)
     dfdx = torch.autograd.grad(wf, f_params["x"], torch.ones_like(wf))[0]
