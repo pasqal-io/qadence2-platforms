6fd26f55a92d748e765add74fff7f582a86939cd
Kaonan Micadei <k.micadei@gmail.com>
2024-07-31 08:10:58 +0200

    clear parameters order in `compile_to_backend` function

--- qadence2_platforms/compiler.py
+++ qadence2_platforms/compiler.py
@@ -6,6 +6,6 @@ from qadence2_ir.types import Model
 from .abstracts import AbstractInterface as Interface
 
 
-def compile_to_backend(model: Model, platform: str) -> Interface:
+def compile_to_backend(platform: str, model: Model) -> Interface:
     plat = import_module(f"qadence2_platforms.backend.{platform}")
     return plat.compile_to_backend(model)
